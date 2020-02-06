0b223b650183e198e45aab97559e01f3d2ee41c5
Cserna917 <bcserna@ilstu.edu>
2020-02-06 10:15:40 -0600

    yea

--- p5/week3/videoassingment/sketchpic.js
+++ p5/week3/videoassingment/sketchpic.js
@@ -1,11 +1,16 @@
-var carPic;
+var carPic1;
+var carPic2;
+var carPic3;
 
 function setup(){
-createCanvas(2000, 2000);
-carPic = loadImage("assets/19.jpg");
+createCanvas(5000, 5000);
+carPic1 = loadImage("assets/car1.jpg");
+carPic2 = loadImage("assets/car2.jpg");
+carPic3 = loadImage("assets/car3.jpg");
 }
 
 function draw() {
-
-  image(carPic, 20, 100);
+  image(carPic1, 0, 0);
+  image(carPic2, 0, 1000);
+  image(carPic3, 0, 2000);
 }
